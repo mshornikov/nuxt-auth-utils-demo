dfcf5c3000842aa5d4a3f38038a20b8237e1a9b4
Mikhail Shornikov <mikhail.shornikov@mail.ru>
2025-02-19 21:29:39 +0300

    chore: Add ESLint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@nuxt/eslint'],
   css: ['~/assets/css/main.css'],
 });
